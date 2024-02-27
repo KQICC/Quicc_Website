7fae4691d5218e575cd569429444c4db18e40b16
ashwinpra <ashprasanth2003@gmail.com>
2024-02-27 14:39:32 +0530

    wip;

---
+++ src/routes/+page.svelte
@@ -0,0 +1,76 @@
+<script>
+	// import welcome from '$lib/images/svelte-welcome.webp';
+	import logo from '$lib/images/logo-nobg.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<!-- Home section -->
+<section id="home">
+	<h1> Kharagpur Quantum Information and Computing Club </h1>
+	<!-- Logo -->
+	<img src={logo} id="logo" alt="KQICC logo" />
+</section>
+
+<!-- About section -->
+<section id="about">
+<div class="text-column">
+	<h1>About Us</h1>
+
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo inventore itaque voluptatum accusantium? Sapiente incidunt dolore nobis atque aperiam! Nulla quos tenetur id officia doloremque rerum minus odit tempora excepturi possimus minima maxime nobis, eius cum earum debitis inventore dolor.
+	</p>
+
+	<p>
+		Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure architecto maxime quae modi at reiciendis animi, eos quam non debitis a repellat ad culpa, cumque accusantium. Modi error eaque ea!
+	</p>
+
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate deleniti nobis explicabo porro dolor tempora quos quo quia, ea officia.
+	</p>
+</div>
+</section>
+
+<!-- Members section -->
+<section id="members">
+	<div class="text-column">
+		<h1>Members</h1>
+	
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo inventore itaque voluptatum accusantium? Sapiente incidunt dolore nobis atque aperiam! Nulla quos tenetur id officia doloremque rerum minus odit tempora excepturi possimus minima maxime nobis, eius cum earum debitis inventore dolor.
+		</p>
+	
+		<p>
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure architecto maxime quae modi at reiciendis animi, eos quam non debitis a repellat ad culpa, cumque accusantium. Modi error eaque ea!
+		</p>
+	
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate deleniti nobis explicabo porro dolor tempora quos quo quia, ea officia.
+		</p>
+	</div>
+</section>
+
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	section h1 {
+		font-size: 2rem;
+		text-align: center;
+		margin-bottom: 0rem;
+		color: var(--color-logo-secondary);
+	}
+
+	#logo {
+		width: 70%;
+		height: auto;
+	}
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,100 @@
+<script lang=ts>
+	import './styles.css';
+
+	function handleSectionClick(event: Event) {
+		event.preventDefault();
+		try {
+			const link = event.currentTarget as HTMLAnchorElement;
+			const anchorId = new URL(link.href).hash.slice(1);
+			const anchor = document.getElementById(anchorId);
+			if (anchorId === "home") {
+				scrollTo({ top: 0, behavior: "smooth" });
+			} else {
+				scrollTo({ top: anchor.offsetTop - 50, behavior: "smooth" });
+			}
+		} catch (error) {
+			console.error(error);	
+		}
+	}
+</script>
+
+<header>
+	<nav>
+		<ul>
+			<li>
+				<a href="#home" on:click={handleSectionClick}>Home</a>
+			</li>
+			<li>
+				<a href="#about" on:click={handleSectionClick}>About</a>
+			</li>
+			<li>
+				<a href="#members" on:click={handleSectionClick}>Members</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		justify-content: space-between;
+		position: sticky;
+		top: 0;
+		z-index: 100; 
+		/* todo change bg color */
+		background: var(--color-logo-bg); 
+	}
+
+	nav {
+		justify-content: center;
+		--background: rgba(255, 255, 255, 1);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--color-logo-bg);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	/* !todo -> fix this */
+	/* li[aria-current='page']::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--color-theme-1);
+	} */
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 0.5rem;
+		color: var(--color-logo-primary);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--color-logo-tertiary);
+	}
+</style>
