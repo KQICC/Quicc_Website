b61fe4b599a39b5bff32fb0579a04ee3e07dbcb8
Pranshu Dasgupta <pranshu.dg@gmail.com>
2024-09-20 00:34:17 +0530

    Temporary website folder

---
+++ web_kqicc_temp/script.js
@@ -0,0 +1,6 @@
+<div class="text-container">
+    <h2>Event Schedule</h2>
+    <p>
+        Quantum Week starts October 7th with a series of hybrid events aimed at exploring advancements in quantum computing.
+    </p>
+</div>
